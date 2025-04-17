207132f0acf8d0a1920f8e1d696313b6d6f21b69
Owuraku <boatseth3@gmail.com>
2025-04-17 12:40:34 +0000

    added route protection

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('services', ServiceController::class);
+Route::resource('services', ServiceController::class)->middleware('auth');
 
 Route::prefix('auth')->controller(AuthController::class)->name('auth.')
     ->group(function () {
-        Route::get('login', 'loginPage')->name('login.page');
-        Route::post('login', 'login')->name('login');
+        Route::get('login', 'loginPage')->name('login.page')->middleware('guest');
+        Route::post('login', 'login')->name('login')->middleware('guest');
 
-        Route::get('register', 'registerPage')->name('register.page');
-        Route::post('register', 'register')->name('register');
+        Route::get('register', 'registerPage')->name('register.page')->middleware('guest');
+        Route::post('register', 'register')->name('register')->middleware('guest');
     });
